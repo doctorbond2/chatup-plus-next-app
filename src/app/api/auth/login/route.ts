b984b07dddca617e101cbafc0f5db6438c6b141e
doctorbond2 <michael.rostlund@hotmail.com>
2024-10-03 17:12:16 +0200

    Refactor login route to use typed return value in POST function

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,6 +1,6 @@
 import { LoginUser } from '@/utils/handlers/Auth/post';
 import { NextRequest } from 'next/server';
-export async function POST(request: NextRequest) {
+export async function POST(request: NextRequest): Promise<Response> {
   console.log('Login route');
   return await LoginUser(request);
 }
--- src/app/api/auth/refreshToken/route.ts
+++ src/app/api/auth/refreshToken/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from 'next/server';
 import { refreshTokens } from '@/utils/handlers/Auth/post';
-export async function POST(request: NextRequest) {
+export async function POST(request: NextRequest): Promise<Response> {
   return await refreshTokens(request);
 }
